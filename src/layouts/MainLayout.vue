00e73a74812c9fd398599514518c199ef1771622
cho <crisoberyl@naver.com>
2020-05-19 16:58:05 +0900

    뭘까?

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,20 @@
 <template>
   <q-layout view="hHh LpR fff">
-
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="left = !left" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
+          </q-avatar>안녕하세여
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer show-if-above v-model="left" side="left" bordered>
       <!-- drawer content -->
+      <router-view />
     </q-drawer>
 
     <q-page-container>
@@ -26,13 +25,11 @@
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
+          </q-avatar>시리시우네
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
 
@@ -40,17 +37,17 @@
 // import EssentialLink from 'components/EssentialLink'
 
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     // EssentialLink
   },
 
-  data () {
+  data() {
     return {
       // leftDrawerOpen: false,
       left: false
-    }
+    };
   }
-}
+};
 </script>
