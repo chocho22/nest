0c32551c27a3431b92a01e23ca0fa53acdd78634
cho <crisoberyl@naver.com>
2020-05-21 22:48:51 +0900

    11111

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="q-pa-xl q-pb-md q-mx-auto float-left" style="width:500px;">
       <q-card clickable class="my-card">
-        <q-img src="~assets/cat/시리.jpg" basic>
+        <q-img src="~assets/cat/시리.jpg" style="height: 140px; max-width: 150px" basic>
           <div class="absolute-bottom text-h6">시리</div>
         </q-img>
 
@@ -11,7 +11,7 @@
     </div>
     <div class="q-pa-xl q-pb-md q-mx-auto" style="width:500px; display:inline-block;">
       <q-card clickable class="my-card">
-        <q-img src="~assets/cat/시우.jpg" basic>
+        <q-img style="height: 140px; max-width: 150px" src="~assets/cat/시우.jpg" basic>
           <div class="absolute-bottom text-h6">시우</div>
         </q-img>
 
@@ -42,4 +42,58 @@ export default {
   height: 100%;
   // background-image: url("~assets/고양이.jpg");
 }
+
+//https://webclub.tistory.com/227
+body {
+  margin: 100px;
+}
+
+.container {
+  position: relative;
+  margin: 10px 0 20px 0;
+  perspective: 1000;
+}
+
+.card {
+  width: 200px;
+  height: 50px;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: 0.5s;
+}
+
+.face {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  color: white;
+  line-height: 50px;
+  text-align: center;
+  backface-visibility: hidden;
+}
+
+.front {
+  background: red;
+}
+
+.back {
+  transform: rotateY(180deg);
+  background: green;
+/*   backface-visibility:hidden; */
+}
+
+.container:hover .card {
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  -ms-transform: rotateY(180deg);
+  -o-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+}
+
+.container:hover .front {
+/*   z-index: 0; */
+}
+
 </style>
